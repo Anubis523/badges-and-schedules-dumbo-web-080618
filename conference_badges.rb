30bf117023f9600912f43f5738d654b1fbd4bf78
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 13:47:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ def assign_rooms (names)
 end
 
 def printer (names)
-  batch_badge_creator(names).each{}
-  puts assign_rooms(names)
+  batch_badge_creator(names).each{|x| puts x}
+  assign_rooms(names).each{|x| puts x}
 end
