802cceddd00be7ba50752b272449cc75b7f25de8
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 12:54:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def batch_badge_creator (names)
 end
 
 def assign_rooms (names)
-  names.each_with_index{|name, index| }
+  names.each_with_index{|name, index| "Hello #{name}! You'll be assigned to room #{index+1}!"}
 end
